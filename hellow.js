8febb9b8a641a841099a01ba2646d5d7c28d437b
mayurit1510 <mayurit1510@gmail.com>
2023-05-05 16:19:22 +0530

    create hellow.js
    create hellow.js

---
+++ hellow.js
@@ -0,0 +1,19 @@
+
+
+
+
+console.log("Mayuri Tandel");
+
+var firstName = "Mayuri"; 
+console.log(firstName);
+
+var age; //Variable declaration
+age = "22"; //variable inialization
+console.log(age);
+
+var holder;    //Variable declaration
+console.log(holder); //undefined
+holder = "My Spec";
+console.log(holder);
+holder = "Sachin Spec" //variable update
+console.log(holder);
