27bf9ce14c8f640804e46325f6ee9060850a4f07
mayurit1510 <mayurit1510@gmail.com>
2023-05-11 16:41:49 +0530

    commit
    commit

---
+++ 03_typeOf.js
@@ -0,0 +1,32 @@
+console.log("---before Initialization");
+var bankName;
+console.log( "Bank name is:", bankName , "Data type is:", typeof bankName);
+
+console.log("---after Initialization");
+var bankName="SBI Bank";
+console.log( "Bank name is:", bankName , "Data type is:", typeof bankName);
+
+console.log("---type of marks--");
+var marks= '90% ';
+console.log( "Marks:", marks , " type  of marks is:", typeof marks);
+
+console.log("---before Initialization");
+var isWorking;
+console.log( "isWorking:", isWorking , "type is:", typeof isWorking);
+
+console.log("---after Initialization");
+var isWorking=true;
+console.log( "isWorking:", isWorking , "type is:", typeof isWorking);
+
+var totalCount="Hundred and seven";
+console.log("totalCount:",totalCount , "type" ,typeof totalCount);
+var totalCount= 107;
+console.log("totalCount:",totalCount , "type" ,typeof totalCount);
+
+
+
+
+
+
+
+
