f9099aba81a51ba4a0a181b3dad538c48a491552
mayurit1510 <mayurit1510@gmail.com>
2023-05-11 16:42:57 +0530

    commit
    commit

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,31 @@
+console.log("---Number---");
+var salary=100000;
+var month= 12;
+var PF= 1200;
+console.log("Salary :",salary  ,"Type:" ,typeof salary);
+console.log("Month :",month ,"Type:" ,typeof month);
+console.log("PF :",PF ,"Type:" ,typeof PF);
+
+console.log("---String---");
+var employeeName= "Durva" ;
+var employeeLastName= "Temkar";
+var address= "Pune";
+console.log("Employee Name :",salary  ,"Type:" ,typeof employeeName);
+console.log("Employee lastname :",salary  ,"Type:" ,typeof employeeLastName);
+console.log("Address :",salary  ,"Type:" ,typeof address);
+
+console.log("---Boolean---");
+var isTeamMember=false;
+var isSoftwareEngineer =true;
+var knowAngular= true;
+console.log("isTeamMember:",salary  ,"Type:" ,typeof isTeamMember);
+console.log("isSoftwareEngineer:",salary  ,"Type:" ,typeof isSoftwareEngineer);
+console.log("knowAngular:",salary  ,"Type:" ,typeof knowAngular);
+
+console.log("---Difference null vs undefined---");
+
+var age=null;
+console.log("variable declared as null :",age , " type:" ,typeof age);
+var weight;
+console.log("variable is not initialize :" ,weight, "type",typeof weight);
+
