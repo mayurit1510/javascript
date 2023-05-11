7c963d715e8d33758950d2b5f06d26ff20cff454
mayurit1510 <mayurit1510@gmail.com>
2023-05-11 17:34:28 +0530

    commit
    commit

--- 03_function.js
+++ 03_function.js
@@ -10,9 +10,14 @@ console.log("after function");
 
 var num1=100;
 var num2=200;
+
 function swapVariables(value1,value2) { //arguments
-    console.log(value1,value2);
-   // var value1=100
-   // var value2=200
+    
+console.log("Before Swap:",value1,value2);
+var temp=value1;
+value1=value2;
+value2=temp;
+console.log("After Swap",value1,value2);
+   
 }
 swapVariables(num1,num2);//100,200
