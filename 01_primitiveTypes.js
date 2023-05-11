1274c75b0f3770de7d966c071942a53b17c216c6
mayurit1510 <mayurit1510@gmail.com>
2023-05-11 16:42:13 +0530

    commit
    commit

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,44 @@
+
+
+
+
+//primitive type
+var age = 31;
+var weight = 78;
+var height = 6.7;
+var speckNumber = 0.50;
+console.log(age,weight,height,speckNumber);
+console.log( "Age =",age);
+console.log(weight);
+console.log(height);
+console.log(speckNumber);
+
+ var typeofAge= typeof age; //typeof
+ console.log(typeofAge);
+
+//2 .Boolean ---> true or false
+var isMarried = true;
+var knowAngular = true;
+var isSoftwareEngineer = true;
+console.log(isMarried);
+
+var typeofIsMarried= typeof isMarried; //typeof
+console.log("Type of variable--> isMarried" ,typeofIsMarried);
+
+//3. String --> Sequence of character enclosed in '' ""
+var firstName= " Bill";
+var position = " Microsoft Co-Founder";
+console.log(firstName);
+console.log(position);
+var typeofFirstName= typeof firstName;
+console.log(" Type of variable--> firstName",typeofFirstName);
+
+//Special Data Type
+var address;   //type of undefined variable--->undefined
+var typeofAddress= typeof address;
+console.log("type of undefined variable",typeofAddress);
+//2.null
+var pin=null;
+console.log("value of variable",pin);
+console.log("type of pin is ",typeof pin);
+
--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -4,3 +4,29 @@
 
 var myFullName;
 console.log(myFullName);
+
+myFullName = "Mayuri Tandel.";
+console.log(myFullName);
+
+myFullName = "Mayuri Mangesh Tandel.";
+console.log(myFullName);
+
+var myLove;
+myLove ="I love only JS";
+console.log(myLove);
+
+console.log("Mayuri");
+console.log("Tandel");
+console.log("25");
+console.log("Hadapsar,pune");
+console.log("471120");
+console.log("Female");
+console.log("Unmarried");
+
+
+
+
+
+
+
+
