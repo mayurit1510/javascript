81a112724be0a8eadbc776dd4c0188d17038202f
mayurit1510 <mayurit1510@gmail.com>
2023-05-11 16:43:51 +0530

    create swap variable file
    swap two string and three no

---
+++ 02_swapVariables.js
@@ -0,0 +1,19 @@
+
+
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("a).before swap","sweety",sweety ,"cutie",cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("b).after swap","sweety",sweety ,"cutie",cutie);
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("a).before swap","num1",num1 ,"num2",num2,"num3",num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("b).after swap","num1",num1 ,"num2",num2,"num3",num3);
