c2c1fc629a13fc33a4380edaab60fd60101ee1db
mayurit1510 <mayurit1510@gmail.com>
2023-05-22 08:15:07 +0530

    commited
    commited

---
+++ 07_ifStatement.js
@@ -0,0 +1,20 @@
+
+
+
+console.log("I am going to office");
+var isTrafficToDailyRoute=true;
+if(isTrafficToDailyRoute){
+console.log("Daily route");
+console.log("Going by cab");
+console.log("Had breakfast in middle");
+
+}
+console.log("Office");
+
+var num1=13;
+var isEven=num1%2==0;
+if(isEven){
+console.log(`Is Even ${isEven},Even number:${num1}`);
+}else{
+    console.log(`Is Even ${isEven},Odd number:${num1}`);
+}console.log("After if else statement");
